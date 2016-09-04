10df66447372f1c5e517a9f9befa7392ed2042f4
Alexander Budyak <ambudyak@gmail.com>
2016-09-04 22:53:42 +0300

    module3.homework.task1 has been added

---
+++ src/module3/homework/task1/Animal.java
@@ -0,0 +1,9 @@
+package module3.homework.task1;
+
+
+public class Animal {
+
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
---
+++ src/module3/homework/task1/Solution.java
@@ -0,0 +1,15 @@
+package module3.homework.task1;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Bird myByrd = new Bird();
+
+        myByrd.walk();
+        myByrd.fly();
+        myByrd.sing();
+        myByrd.whoAmI();
+
+    }
+}
