4bf786e4db21b0bfb5c57ab8839ca6df7106e3e0
Alexander Budyak <ambudyak@gmail.com>
2016-09-04 23:26:35 +0300

    module3.homework.task2 has been added

---
+++ src/module3/homework/task2/Adder.java
@@ -0,0 +1,10 @@
+package module3.homework.task2;
+
+
+public class Adder extends Arithmetic {
+
+    boolean check(Integer a, Integer b) {
+        return (a >= b ? true : false);
+
+    }
+}
---
+++ src/module3/homework/task2/Arithmetic.java
@@ -0,0 +1,11 @@
+package module3.homework.task2;
+
+
+public class Arithmetic {
+
+    int add(Integer a, Integer b) {
+        return (a + b);
+    }
+
+}
+
