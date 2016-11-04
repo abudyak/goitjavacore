ac8e4e7ac627658fbc27ef18485d43aebbd19396
Alexander Budyak <ambudyak@gmail.com>
2016-11-05 01:07:03 +0200

    finalProject is started

---
+++ src/finalProject/Controller.java
@@ -0,0 +1,36 @@
+package finalProject;
+
+
+public class Controller {
+
+    /*
+    Project: hotel online booking
+
+    You need you create system which serves following user requests
+
+
+    //Hotel - Collection<Room>
+    //Room - User userReserved
+    Collection<Hotel> findHoteByName(String name)
+    Collection<Hotel> findHotelByCity(String city)
+    void bookRoom(long roomId, long userId, long hotelId)
+    void cancelReservation(long roomId, long userId, long hotelId)
+    Collection<Room> findRoom(Map<Sring, String> params)
+
+
+    //city - Kiev, hotelName - Radisson, price - 200, persons - 2
+
+        registerUser(User user)
+
+    - There are three main entities in the system, User, Hotel and Room
+    - They should be stored in separate table (collection)
+    - Abstract DAO should be implemented and used for duplicate operations
+    - all request should be placed in Controller
+    - use java 8 where possible
+    - Demonstrate all results in Main class (create and least 2 hotels in 3 unique cities with at least 10 rooms each)
+    */
+
+
+
+
+}
